124b968862a31255df286179e746324450ffd719
aportekila <abdullahakgul70@gmail.com>
2025-09-29 13:54:42 +0000

    Deploying to gh-pages from @ aportekila/aportekila.github.io@3b92a2985b0f36b4176e53282feacc29bf1b6ee8 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -5,11 +5,11 @@ function determineGiscusTheme() {
       document.documentElement.getAttribute("data-theme") ||
       "system";
 
-    if (theme === "dark") return "dark";
-    if (theme === "light") return "light";
+    if (theme === "dark") return "gruvbox";
+    if (theme === "light") return "gruvbox_light";
 
     const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    return prefersDark ? "dark" : "light";
+    return prefersDark ? "gruvbox" : "gruvbox_light";
   
 }
 
