ca28879a057e58fd1082dbdd260149cbdb5ead73
aportekila <abdullahakgul70@gmail.com>
2026-07-31 17:35:00 +0000

    Deploying to gh-pages from @ aportekila/aportekila.github.io@1fe1942584bc1f74945a7beb549fc62bb8837e6d 🚀

--- assets/js/google-analytics-setup.js
+++ assets/js/google-analytics-setup.js
@@ -1,6 +1,26 @@
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  window.dataLayer.push(arguments);
-}
-gtag("js", new Date());
-gtag("config", "G-GNTR46DBKW");
+(function () {
+  var GA_ID = "G-GNTR46DBKW";
+
+  window.__loadGoogleAnalytics = function () {
+    if (window.__googleAnalyticsLoaded) return;
+    window.__googleAnalyticsLoaded = true;
+
+    var lib = document.createElement("script");
+    lib.async = true;
+    lib.src = "https://www.googletagmanager.com/gtag/js?id=" + GA_ID;
+    document.head.appendChild(lib);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    window.gtag = gtag;
+    gtag("js", new Date());
+    gtag("config", GA_ID);
+  };
+
+  // Returning visitor who already granted consent on a previous visit.
+  if (localStorage.getItem("analytics-consent") === "granted") {
+    window.__loadGoogleAnalytics();
+  }
+})();
